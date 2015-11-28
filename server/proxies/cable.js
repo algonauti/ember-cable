44717f8ce4eabf8e663b87781c583ce4459f2bfa
Salvatore Ferrucci <salvatore@algonauti.com>
2015-11-28 11:50:39 +0100

    add http-proxy and forward /cable to backend.
    In order to test dummy application with actioncable, we need
    to proxy websocket requests to the backend. In this dummy
    application the endpoint is /cable and the server running at
    http://localhost:3000.

---
+++ server/proxies/cable.js
@@ -0,0 +1,24 @@
+/*jshint node:true*/
+var proxyPath = '/cable';
+
+module.exports = function(app, options) {
+  var server = options.httpServer;
+  var proxy = require('http-proxy').createProxyServer({
+    target: 'http://localhost:3000',
+    ws: true,
+    changeOrigin: true
+  });
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  server.on('upgrade', function (req, socket, head) {
+    proxy.ws(req, socket, head);
+  });
+  
+  app.use(proxyPath, function(req, res, next){
+    req.url = proxyPath + '/' + req.url;
+    proxy.web(req, res);
+  });
+};
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,6 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    
+    contentSecurityPolicy: {
+      'connect-src': "ws://localhost:4200/cable"
     }
   };
 
