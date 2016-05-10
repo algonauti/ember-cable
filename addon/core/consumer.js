a2edb9f7be7e2d7edb2321dbeb2b3de139d2a54f
iggor_kurr <kurinnoyi@gmail.com>
2016-05-11 00:57:20 +0300

    added simple queue to consumer
    This prevents from sending to connection when it is in CONNECTING state

--- addon/core/consumer.js
+++ addon/core/consumer.js
@@ -4,13 +4,33 @@ import Connection from 'ember-cable/core/connection';
 
 export default Ember.Object.extend({
   url: null,
+  queue: null,
 
   setupConnection: Ember.on('init', function() {
     this.set('subscriptions', Subscriptions.create({ consumer: this }));
     this.set('connection', Connection.create({ consumer: this }));
+    this.set('queue', []);
   }),
 
   send(data) {
-    this.get('connection').send(data);
-  }
+    this.get('queue').push(data);
+  },
+
+  queueObserver: Ember.on('init', Ember.observer('queue.[]', 'connection.connected', function() {
+    const connection = this.get('connection');
+    const queue = this.get('queue');
+
+    if (!connection || !connection.get('connected')) {
+      return;
+    }
+
+    if (Ember.isEmpty(queue)) {
+      return;
+    }
+
+    queue.forEach((data, index) => {
+      connection.send(data);
+      queue.splice(index, 1);
+    });
+  }))
 });
