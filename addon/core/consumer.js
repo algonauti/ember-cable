b868e2a03545ff454d0f67a0efedc8fcd8d0dcaf
Salvatore Ferrucci <salvatore@algonauti.com>
2016-06-05 22:02:16 +0200

    remove queue from consumer
    for some reason after a2edb9f the consumer stop
    to send data to connection.

--- addon/core/consumer.js
+++ addon/core/consumer.js
@@ -4,33 +4,15 @@ import Connection from 'ember-cable/core/connection';
 
 export default Ember.Object.extend({
   url: null,
-  queue: null,
 
   init() {
+    this._super(...arguments);
     this.set('subscriptions', Subscriptions.create({ consumer: this }));
     this.set('connection', Connection.create({ consumer: this }));
-    this.set('queue', []);
   },
 
   send(data) {
-    this.get('queue').pushObject(data);
-  },
+    this.get('connection').send(data);
+  }
 
-  queueObserver: Ember.on('init', Ember.observer('queue.[]', 'connection.connected', function() {
-    const connection = this.get('connection');
-    const queue = this.get('queue');
-
-    if (!connection || !connection.get('connected')) {
-      return;
-    }
-
-    if (Ember.isEmpty(queue)) {
-      return;
-    }
-
-    queue.forEach((data, index) => {
-      connection.send(data);
-      queue.splice(index, 1);
-    });
-  }))
 });
