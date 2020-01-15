39d48d46ee6f72ba1bfadc0629913d0fbf4e3c28
Salvatore Ferrucci <salvatore@algonauti.com>
2020-01-15 18:54:10 +0100

    notification-messages implement a real example

--- tests/dummy/app/components/notification-messages/component.js
+++ tests/dummy/app/components/notification-messages/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { debug, inspect } from '@ember/debug';
 
 export default Component.extend({
   cable: service('cable'),
@@ -10,6 +11,34 @@ export default Component.extend({
   },
 
   _setupConsumer() {
-    this.get('cable').createConsumer('ws://localhost:4200/cable');
-  }
+    let consumer = this.cable.createConsumer('ws://localhost:4200/cable');
+
+    consumer.createSubscription('BroadcastChannel', {
+      connected() {
+        debug('BroadcastChannel#connected');
+        this.perform('ping');
+      },
+      received(data) {
+        debug( "received(data) -> " + inspect(data) );
+      },
+      disconnected() {
+        debug("BroadcastChannel#disconnected");
+      }
+    });
+
+    // Passing Parameters to Channel
+    consumer.createSubscription({ channel: 'BroadcastChannel', room: 'BestRoom' }, {
+      connected() {
+        this.perform('ping', { foo: 'bar' });
+      },
+
+      received: (data) => {
+        this._updateRecord(data);
+      }
+    });
+  },
+
+  _updateRecord(data) {
+     debug( "updateRecord(data) -> " + inspect(data) );
+   }
 });
