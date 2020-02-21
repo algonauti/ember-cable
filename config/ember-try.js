882bec74bd214cc407b914275633837dd82292db
Salvatore Ferrucci <salvatore@algonauti.com>
2020-02-21 18:26:04 +0100

    up to ember-cli 3.16.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,21 +4,20 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
   return {
-    useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.4',
+        name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
-            'ember-source': '~3.4.0'
+            'ember-source': '~3.12.0'
           }
         }
       },
       {
-        name: 'ember-lts-3.8',
+        name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
-            'ember-source': '~3.8.0'
+            'ember-source': '~3.16.0'
           }
         }
       },
@@ -46,7 +45,7 @@ module.exports = async function() {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `yarn test`,
+      // The default `.travis.yml` runs this scenario via `npm test`,
       // not via `ember try`. It's still included here so that running
       // `ember try:each` manually or from a customized CI config will run it
       // along with all the other scenarios.
@@ -66,6 +65,21 @@ module.exports = async function() {
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1'
+          }
+        }
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false
+          })
+        },
+        npm: {
+          ember: {
+            edition: 'classic'
           }
         }
       }
