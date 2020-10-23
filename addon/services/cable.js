de0b952e96f39889c208e537a798339bd1c5e7ce
Salvatore Ferrucci <salvatore@algonauti.com>
2020-10-23 09:35:56 +0200

    fix tests

--- addon/services/cable.js
+++ addon/services/cable.js
@@ -18,7 +18,7 @@ export default class CableService extends Service {
   }
 
   willDestroy() {
-    this.super();
+    super.willDestroy();
     this._consumers.forEach(consumer => consumer.destroy());
   }
 }
